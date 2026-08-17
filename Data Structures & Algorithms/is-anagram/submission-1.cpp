cc2f5d2863806ffff03cd8c5f1ef2b38694035a0
ihteshamgoraya <gorayaihtesham@gmail.com>
2026-08-17 21:17:40 +0500

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,11 @@
+#include<algorithm>
+#include<string>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if (s != t) return false;
+        return true;
+    }
+};
